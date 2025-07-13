f8146513be1fcd5e70dbf9ea87f0df7cc3f803aa
SwarajWadkar <swaraj.x.wadkar@gmail.com>
2025-07-13 13:45:37 +0530

    Initial commit: video encoding backend using FastAPI, Celery, PostgreSQL, Docker

--- worker/worker.py
+++ worker/worker.py
@@ -1,3 +1,9 @@
+import sys
+import os
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+
 from app.tasks import celery
 
 celery.worker_main(["worker", "--loglevel=info"])
